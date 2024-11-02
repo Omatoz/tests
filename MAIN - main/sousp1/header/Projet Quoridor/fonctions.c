6264b43089a6d26b871f056a6b9e2b29f371f3af
Omatoz <cazau.thomas12@gmail.com>
2024-11-02 14:19:32 +0100

    Add files via upload

---
+++ MAIN - main/sousp1/header/Projet Quoridor/fonctions.c
@@ -0,0 +1,339 @@
+#include <stdio.h>
+#include "header.h"
+
+// Définition des variables globales
+int modePlacement1 = 0;
+int modePlacement2 = 0;
+int modePlacement3 = 0;
+int modePlacement4 = 0;
+int barrierX, barrierY;
+char orientation = 'V'; // Orientation par défaut de la barrière
+
+void initialiserPlateau(char plateau[SIZE][SIZE][4]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            plateau[i][j][0] = '\0';
+
+            if (i == 0 && j == 0) {
+                strcpy(plateau[i][j], TOP_LEFT_CORNER);
+            } else if (i == 0 && j == SIZE - 1) {
+                strcpy(plateau[i][j], TOP_RIGHT_CORNER);
+            } else if (i == SIZE - 1 && j == 0) {
+                strcpy(plateau[i][j], BOTTOM_LEFT_CORNER);
+            } else if (i == SIZE - 1 && j == SIZE - 1) {
+                strcpy(plateau[i][j], BOTTOM_RIGHT_CORNER);
+            } else if (i == 0 || i == SIZE - 1) {
+                strcpy(plateau[i][j], HORIZONTAL_LINE);
+            } else if (j == 0 || j == SIZE - 1) {
+                strcpy(plateau[i][j], VERTICAL_LINE);
+            }
+                // Intérieur du plateau
+            else if (i % 2 == 0 && j % 2 == 0) {
+                strcpy(plateau[i][j], INTERSECTION);
+            } else if (i % 2 == 0) {
+                strcpy(plateau[i][j], HORIZONTAL_LINE);
+            } else if (j % 2 == 0) {
+                strcpy(plateau[i][j], VERTICAL_LINE);
+            } else {
+                strcpy(plateau[i][j], " ");
+            }
+        }
+    }
+}
+
+void afficherPlateau(char plateau[SIZE][SIZE][4], int barrierX, int barrierY, int modePlacement1, int modePlacement2, int modePlacement3, int modePlacement4, char orientation) {
+#ifdef _WIN32
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+#endif
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            char *c = plateau[i][j];
+            int isBarrierTemp = 0;
+
+            // Afficher la barrière temporaire si le joueur est en mode placement
+            if ((modePlacement1 || modePlacement2 || modePlacement3 || modePlacement4)) {
+                if (orientation == 'V') {
+                    if ((i == barrierX - 1 || i == barrierX || i == barrierX + 1 || i == barrierX + 2) && j == barrierY) {
+                        isBarrierTemp = 1;
+                    }
+                } else if (orientation == 'H') {
+                    if (i == barrierX && (j == barrierY - 1 || j == barrierY || j == barrierY + 1 || j == barrierY + 2)) {
+                        isBarrierTemp = 1;
+                    }
+                }
+            }
+
+            if (strcmp(c, "1") == 0) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+                printf("o ");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[34mo\033[0m ");
+#endif
+            } else if (strcmp(c, "2") == 0) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+                printf("o ");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[31mo\033[0m ");
+#endif
+            } else if (strcmp(c, "|") == 0 || strcmp(c, "-") == 0) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+                printf("%s ", c);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[32m%s\033[0m ", c);
+#endif
+            } else if (strcmp(c, "3") == 0) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+                printf("o ");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[32mo\033[0m ");
+
+#endif
+            } else if (strcmp(c, "4") == 0) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+                printf("o ");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[33mo\033[0m ");
+#endif
+            } else if (isBarrierTemp) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+                printf("%s ", orientation == 'V' ? "|" : "-");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[32;1m%s\033[0m ", orientation == 'V' ? "|" : "-");
+#endif
+            } else if (i == 0 || i == SIZE - 1 || j == 0 || j == SIZE - 1) {
+#ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN);
+                printf("%s ", c);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+                printf("\033[0;33m%s\033[0m ", c);
+#endif
+            } else {
+                printf("%s ", c);
+            }
+        }
+        printf("\n");
+    }
+}
+
+#ifdef _WIN32
+int lireTouche() {
+    int ch = getch();
+    if (ch == 0 || ch == 224) {
+        ch = getch();
+        switch (ch) {
+            case 72: return 'U'; // Flèche haut
+            case 80: return 'D'; // Flèche bas
+            case 75: return 'L'; // Flèche gauche
+            case 77: return 'R'; // Flèche droite
+            default: return 0;
+        }
+    } else {
+        return ch; // Retourne le caractère tel quel pour les autres touches
+    }
+}
+#else
+void initialiserTermios(int echo) {
+    struct termios newt;
+    tcgetattr(STDIN_FILENO, &newt);
+    newt.c_lflag &= ~ICANON; // Mode non canonique
+    newt.c_lflag &= echo ? ECHO : ~ECHO; // Activer/désactiver l'écho
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+}
+
+void reinitialiserTermios() {
+    struct termios newt;
+    tcgetattr(STDIN_FILENO, &newt);
+    newt.c_lflag |= ICANON;
+    newt.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, &newt);
+}
+
+int lireTouche() {
+    int ch = getchar();
+    if (ch == '\033') { // Séquence d'échappement
+        getchar(); // Sauter le '['
+        switch(getchar()) {
+            case 'A': return 'U'; // Flèche haut
+            case 'B': return 'D'; // Flèche bas
+            case 'C': return 'R'; // Flèche droite
+            case 'D': return 'L'; // Flèche gauche
+            default: return 0;
+        }
+    } else {
+        return ch;
+    }
+}
+#endif
+
+int barriereEntre(int x1, int y1, int x2, int y2, char plateau[SIZE][SIZE][4]) {
+    int midX = (x1 + x2) / 2;
+    int midY = (y1 + y2) / 2;
+    if (strcmp(plateau[midX][midY], "|") == 0 || strcmp(plateau[midX][midY], "-") == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+void deplacerPion(char plateau[SIZE][SIZE][4], int *x, int *y, int direction, char joueur) {
+    int newX = *x, newY = *y;
+
+    if (joueur == PLAYER1) {
+        if (direction == 'z' && *x > 1) {
+            newX -= 2;
+        } else if (direction == 's' && *x < SIZE - 2) {
+            newX += 2;
+        } else if (direction == 'q' && *y > 1) {
+            newY -= 2;
+        } else if (direction == 'd' && *y < SIZE - 2) {
+            newY += 2;
+        } else {
+            return;
+        }
+    } else if (joueur == PLAYER2) {
+        if (direction == 'U' && *x > 1) {
+            newX -= 2;
+        } else if (direction == 'D' && *x < SIZE - 2) {
+            newX += 2;
+        } else if (direction == 'L' && *y > 1) {
+            newY -= 2;
+        } else if (direction == 'R' && *y < SIZE - 2) {
+            newY += 2;
+        } else {
+            return;
+        }
+    } else if (joueur == PLAYER3) {
+        if (direction == 't' && *x > 1) {
+            newX -= 2;
+        } else if (direction == 'g' && *x < SIZE - 2) {
+            newX += 2;
+        } else if (direction == 'f' && *y > 1) {
+            newY -= 2;
+        } else if (direction == 'h' && *y < SIZE - 2) {
+            newY += 2;
+        } else {
+            return;
+        }
+    } else if (joueur == PLAYER4) {
+        if (direction == 'i' && *x > 1) {
+            newX -= 2;
+        } else if (direction == 'k' && *x < SIZE - 2) {
+            newX += 2;
+        } else if (direction == 'j' && *y > 1) {
+            newY -= 2;
+        } else if (direction == 'l' && *y < SIZE - 2) {
+            newY += 2;
+        } else {
+            return;
+        }
+    }
+
+    if (barriereEntre(*x, *y, newX, newY, plateau)) {
+        return;
+    }
+
+    if (strcmp(plateau[newX][newY], " ") == 0) {
+        strcpy(plateau[*x][*y], " ");
+        *x = newX;
+        *y = newY;
+        char strJoueur[2] = {joueur, '\0'};
+        strcpy(plateau[*x][*y], strJoueur);
+    }
+}
+
+void deplacerBarriere(int *x, int *y, int direction, char joueur) {
+    int deltaX = 0, deltaY = 0;
+
+    if (joueur == PLAYER1) {
+        if (direction == 'z') {
+            deltaX = -1;
+        } else if (direction == 's') {
+            deltaX = 1;
+        } else if (direction == 'q') {
+            deltaY = -1;
+        } else if (direction == 'd') {
+            deltaY = 1;
+        }
+    } else if (joueur == PLAYER2) {
+        if (direction == 'U') {
+            deltaX = -1;
+        } else if (direction == 'D') {
+            deltaX = 1;
+        } else if (direction == 'L') {
+            deltaY = -1;
+        } else if (direction == 'R') {
+            deltaY = 1;
+        }
+    } else if (joueur == PLAYER3) {
+        if (direction == 't') {
+            deltaX = -1;
+        } else if (direction == 'g') {
+            deltaX = 1;
+        } else if (direction == 'f') {
+            deltaY = -1;
+        } else if (direction == 'h') {
+            deltaY = 1;
+        }
+    } else if (joueur == PLAYER4) {
+        if (direction == 'i') {
+            deltaX = -1;
+        } else if (direction == 'k') {
+            deltaX = 1;
+        } else if (direction == 'j') {
+            deltaY = -1;
+        } else if (direction == 'l') {
+            deltaY = 1;
+        }
+    }
+
+    *x += deltaX;
+    *y += deltaY;
+
+    // Garder x et y dans les limites du plateau
+    if (*x < 1) *x = 1;
+    if (*x > SIZE - 3) *x = SIZE - 3; // Ajusté pour barrière de taille 4
+    if (*y < 1) *y = 1;
+    if (*y > SIZE - 3) *y = SIZE - 3; // Ajusté pour barrière de taille 4
+}
+
+void placerBarriere(char plateau[SIZE][SIZE][4], int x, int y, char orientation) {
+    if (orientation == 'V') {
+        if (x > 0 && x < SIZE - 3) {
+            // Placer une barrière verticale (sur quatre cases)
+            strcpy(plateau[x - 1][y], "|");
+            strcpy(plateau[x][y], "|");
+            strcpy(plateau[x + 1][y], "|");
+            strcpy(plateau[x + 2][y], "|");
+        }
+    } else if (orientation == 'H') {
+        if (y > 0 && y < SIZE - 3) {
+            // Placer une barrière horizontale (sur quatre cases)
+            strcpy(plateau[x][y - 1], "-");
+            strcpy(plateau[x][y], "-");
+            strcpy(plateau[x][y + 1], "-");
+            strcpy(plateau[x][y + 2], "-");
+        }
+    }
+}
+
+void nom(char* nomJoueur, int numeroJoueur) {
+    printf("Choisir le nom du joueur %d : ", numeroJoueur);
+    fgets(nomJoueur, numeroJoueur, stdin);
+    size_t len = strlen(nomJoueur);
+    if (len > 0 && nomJoueur[len - 1] == '\n') {
+        nomJoueur[len - 1] = '\0';
+    }
+}
---
+++ MAIN - main/sousp1/header/Projet Quoridor/main.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include "header.h"
+
+int main() {
+    char plateau[SIZE][SIZE][4];
+    int x1 = 1, y1 = 9; // Position de départ du joueur 1 (colonne centrale)
+    int x2 = SIZE - 2, y2 = 9; // Position de départ du joueur 2 (colonne centrale)
+    int x3 = 9, y3 = 1; // Position de départ du joueur 3 (colonne gauche)
+    int x4 = 9, y4 = SIZE - 2; // Position de départ du joueur 4 (colonne droite)
+    int tour = 1;
+    char nomJoueur1[50], nomJoueur2[50], nomJoueur3[50], nomJoueur4[50];
+
+#ifdef _WIN32
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    nom(nomJoueur1, 1);
+    nom(nomJoueur2, 2);
+    nom(nomJoueur3, 3);
+    nom(nomJoueur4, 4);
+
+    initialiserPlateau(plateau);
+    strcpy(plateau[x1][y1], "1");
+    strcpy(plateau[x2][y2], "2");
+    strcpy(plateau[x3][y3], "3");
+    strcpy(plateau[x4][y4], "4");
+
+    while (1) {
+        system("cls"); // Pour Windows
+        // system("clear"); // Pour Unix/Linux
+
+        afficherPlateau(plateau, barrierX, barrierY, modePlacement1, modePlacement2, modePlacement3, modePlacement4, orientation);
+
+        int input;
+        printf("C'est au joueur %d de jouer.\n", tour);
+
+        if (tour == 1) {
+            if (modePlacement1 == 0) {
+                printf("Appuyez sur 'a' pour placer une barrière ou déplacez votre pion avec z/q/s/d.\n");
+            } else {
+                printf("Déplacez la barrière avec z/q/s/d, changez l'orientation avec 'o' et appuyez sur Entrée pour la placer.\n");
+            }
+        } else if (tour == 2) {
+            if (modePlacement2 == 0) {
+                printf("Appuyez sur 'p' pour placer une barrière ou déplacez votre pion avec les flèches.\n");
+            } else {
+                printf("Déplacez la barrière avec les flèches, changez l'orientation avec 'O' et appuyez sur Entrée pour la placer.\n");
+            }
+        } else if (tour == 3) {
+            if (modePlacement3 == 0) {
+                printf("Appuyez sur 'w' pour placer une barrière ou déplacez votre pion avec t/f/g/h.\n");
+            } else {
+                printf("Déplacez la barrière avec t/f/g/h, changez l'orientation avec 'm' et appuyez sur Entrée pour la placer.\n");
+            }
+        } else if (tour == 4) {
+            if (modePlacement4 == 0) {
+                printf("Appuyez sur 'n' pour placer une barrière ou déplacez votre pion avec i/j/k/l.\n");
+            } else {
+                printf("Déplacez la barrière avec i/j/k/l, changez l'orientation avec 'M' et appuyez sur Entrée pour la placer.\n");
+            }
+        }
+
+        input = lireTouche();
+        // printf("Input: %d\n", input); // Décommentez pour le débogage
+
+        if (tour == 1) {
+            if (input == 'a' && modePlacement1 == 0) {
+                modePlacement1 = 1;
+                barrierX = x1;
+                barrierY = y1;
+                orientation = 'V';
+            } else if (modePlacement1 == 1) {
+                if (input == 13 || input == '\n') { // Touche Entrée
+                    placerBarriere(plateau, barrierX, barrierY, orientation);
+                    modePlacement1 = 0;
+                    tour = 2;
+                } else if (input == 'o') {
+                    orientation = (orientation == 'V') ? 'H' : 'V';
+                } else {
+                    deplacerBarriere(&barrierX, &barrierY, input, PLAYER1);
+                }
+            } else {
+                deplacerPion(plateau, &x1, &y1, input, PLAYER1);
+
+                // Vérifier la condition de victoire pour le joueur 1
+                if (x1 == SIZE - 2) {
+                    system("cls"); // Pour Windows
+                    // system("clear"); // Pour Unix/Linux
+                    afficherPlateau(plateau, barrierX, barrierY, modePlacement1, modePlacement2, modePlacement3, modePlacement4, orientation);
+                    printf("Le joueur 1 a gagné !\n");
+                    break;
+                }
+
+                tour = 2;
+            }
+        } else if (tour == 2) {
+            if (input == 'p' && modePlacement2 == 0) {
+                modePlacement2 = 1;
+                barrierX = x2;
+                barrierY = y2;
+                orientation = 'V';
+            } else if (modePlacement2 == 1) {
+                if (input == 13 || input == '\n') { // Touche Entrée
+                    placerBarriere(plateau, barrierX, barrierY, orientation);
+                    modePlacement2 = 0;
+                    tour = 3;
+                } else if (input == 'O') {
+                    orientation = (orientation == 'V') ? 'H' : 'V';
+                } else {
+                    deplacerBarriere(&barrierX, &barrierY, input, PLAYER2);
+                }
+            } else {
+                deplacerPion(plateau, &x2, &y2, input, PLAYER2);
+
+                // Vérifier la condition de victoire pour le joueur 2
+                if (x2 == 1) {
+                    system("cls"); // Pour Windows
+                    // system("clear"); // Pour Unix/Linux
+                    afficherPlateau(plateau, barrierX, barrierY, modePlacement1, modePlacement2, modePlacement3, modePlacement4,orientation);
+                    printf("Le joueur 2 a gagné !\n");
+                    break;
+                }
+                tour = 3;
+            }
+        } else if (tour == 3) {
+            if (input == 'w' && modePlacement3 == 0) {
+                modePlacement3 = 1;
+                barrierX = x3;
+                barrierY = y3;
+                orientation = 'V';
+            } else if (modePlacement3 == 1) {
+                if (input == 13 || input == '\n') { // Touche Entrée
+                    placerBarriere(plateau, barrierX, barrierY, orientation);
+                    modePlacement3 = 0;
+                    tour = 4;
+                } else if (input == 'm') {
+                    orientation = (orientation == 'V') ? 'H' : 'V';
+                } else {
+                    deplacerBarriere(&barrierX, &barrierY, input, PLAYER3);
+                }
+            } else {
+                deplacerPion(plateau, &x3, &y3, input, PLAYER3);
+
+                // Vérifier la condition de victoire pour le joueur 3
+                if (y3 == SIZE - 2) {
+                    system("cls"); // Pour Windows
+                    // system("clear"); // Pour Unix/Linux
+                    afficherPlateau(plateau, barrierX, barrierY, modePlacement1, modePlacement2, modePlacement3, modePlacement4,orientation);
+                    printf("Le joueur 3 a gagné !\n");
+                    break;
+                }
+                tour = 4;
+            }
+        } else if (tour == 4) {
+            if (input == 'n' && modePlacement4 == 0) {
+                modePlacement4 = 1;
+                barrierX = x4;
+                barrierY = y4;
+                orientation = 'V';
+            } else if (modePlacement4 == 1) {
+                if (input == 13 || input == '\n') { // Touche Entrée
+                    placerBarriere(plateau, barrierX, barrierY, orientation);
+                    modePlacement4 = 0;
+                    tour = 1;
+                } else if (input == 'M') {
+                    orientation = (orientation == 'V') ? 'H' : 'V';
+                } else {
+                    deplacerBarriere(&barrierX, &barrierY, input, PLAYER4);
+                }
+            } else {
+                deplacerPion(plateau, &x4, &y4, input, PLAYER4);
+
+                // Vérifier la condition de victoire pour le joueur 4
+                if (y4 == 1) {
+                    system("cls"); // Pour Windows
+                    // system("clear"); // Pour Unix/Linux
+                    afficherPlateau(plateau, barrierX, barrierY, modePlacement1, modePlacement2, modePlacement3, modePlacement4,orientation);
+                    printf("Le joueur 3 a gagné !\n");
+                    break;
+                }
+                tour = 1;
+            }
+        }
+    }
+
+    return 0;
+}
